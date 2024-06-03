ffecd212fe487912413a980a48a2eb2257cd239f
Arfat <arfat3060@gmail.com>
2024-06-03 16:20:36 +0530

    Initialized and Adding Entity Framework

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentScheduler.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ):base(options)
+        {
+            
+        }
+        
+        
+    }
+}
